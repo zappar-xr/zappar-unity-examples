55b856287f90dd0fef01e003a5b28bc75d0b89f0
Zappar <hello@zappar.com>
2021-03-01 14:44:29 +0000

    Examples for Zappar Unity

--- Assets/Plugins/Android/ZCVAndroidRenderPlugin.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Unity/IUnityGraphics.h"
-#include "zappar.h"
-
-static void UNITY_INTERFACE_API OnNativeGLRenderEvent(int eventID);
-extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API zappar_upload_callback_native_gl();
-
-extern "C" void zappar_pipeline_set(zappar_pipeline_t pipeline);
